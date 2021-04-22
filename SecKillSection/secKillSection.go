0da61b9435bd42048329739648c9a83c29f59d84
tuzhipeng <877003243@qq.com>
2021-04-22 14:33:30 +0800

    update

--- SecKillSection/secKillSection.go
+++ SecKillSection/secKillSection.go
@@ -15,7 +15,7 @@ import (
 
 const port = 7777
 var secKillGoodsStockInfo []dao.SecKillGoodsItem
-//var done chan int
+var done chan int
 
 func initSecKillGoodsStockInfo()  {
 	cache.GetHotDataInsertToRedis()
@@ -23,8 +23,8 @@ func initSecKillGoodsStockInfo()  {
 	//for _, goodsItem := range secKillGoodsStockInfo{
 	//	goodsItem.Stock = goodsItem.Stock/3 + 1
 	//}
-	//done = make(chan int, 1)
-	//done <- 1
+	done = make(chan int, 1)
+	done <- 1
 
 }
 
@@ -42,7 +42,8 @@ func localDecrementGoodsStock(iid string) bool  {
 		}
 	}
 	log.Fatal("secKillGoodsStockInfo中没有该商品ID:", iid)
-	return false
+	return false 
+	//return true
 }
 
 func main() {
@@ -63,7 +64,7 @@ func main() {
 }
 
 func secKillOrderHandleFunc(ctx *gin.Context)  {
-	//<-done
+	<-done
 	//全局读写锁
 
 	var postSecKillOrder reqStruct.OrderListItem
@@ -73,21 +74,21 @@ func secKillOrderHandleFunc(ctx *gin.Context)  {
 		res , err :=cache.RemoteDecrementGoodsStock(goodsIid)
 		// 本地预扣库存成功，远程扣取失败，商品售罄
 		if err == nil && res == 0{
-			//done <- 1
+			done <- 1
 			ctx.JSON(http.StatusOK, gin.H{
 				"success":      false,
 				"message": "商品已售罄",
 			})
 		// 本地预扣库存成功，远程扣取也成功，生成订单，返回成功
 		}else if err== nil && res == 1{
-
+		
 			uid, ok := ctx.Get("uid")
 			if !ok {
 				ctx.AbortWithStatus(http.StatusUnauthorized)
 				return
 			}
 			service.GenerateOrderService(uid.(string), []reqStruct.OrderListItem{postSecKillOrder})
-			//done <- 1
+			done <- 1
 			ctx.JSON(http.StatusOK, gin.H{
 				"success":      true,
 				"message": "抢购成功",
@@ -95,7 +96,7 @@ func secKillOrderHandleFunc(ctx *gin.Context)  {
 		}
 		// 本地库存扣取失败，直接返回无货
 	}else {
-		//done <- 1
+		done <- 1
 		ctx.JSON(http.StatusOK, gin.H{
 			"success":      false,
 			"message": "商品已售罄",
